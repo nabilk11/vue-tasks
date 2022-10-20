59b906fb710f9135e5b742605953c11d76b4913b
nabilk11 <khan.nabiln11.com>
2022-10-20 13:14:36 -0400

    created basic Header component and imported to app

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Vue Task Tracker</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+
+<style scoped>
+header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    justify-content: space-between;
+}
+</style>
